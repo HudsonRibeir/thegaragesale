51d80186f95b68b15f2c196323c7abeeb01e89b6
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2024-12-31 15:21:21 -0300

    Enhance product listing and details: updated product information, added quantity display, improved styling, and implemented image zoom functionality. Added launch configuration for VSCode and new closet images.

--- products.js
+++ products.js
@@ -1,74 +1,26 @@
 const produtos = [
     {
         "id": 1,
-        "titulo": "Produto 1",
-        "descricao": "Este é um excelente produto 1.",
-        "valor": 100.00,
+        "titulo": "TV do quarto (pegar descrição)",
+        "descricao": "TV Samsung 43 polegadas, com 4k, com suporte a HDMI, USB, e conexão com internet. (pegar descrição)",
+        "valor": 1000.00,
         "disponivel": true,
-        "dataEntrega": "30/12/2024",
-        "estado": "Novo",
-        "categoria": "Móveis",
-        "imagens": ["bike-hud-1.jpg", "bike-hud-2.jpg", "bike-hud-1.jpg", "foto4.jpg", "bike-hud-1.jpg"],
-        "whatsapp": "https://wa.me/5511999999999"
+        "quantidade": 1,
+        "dataEntrega": "Imediata",
+        "estado": "Usao",
+        "categoria": "Eletrônicos",
+        "imagens": ["bike-hud-1.jpg", "bike-hud-2.jpg", "bike-hud-1.jpg", "foto4.jpg", "bike-hud-1.jpg"]
     },
     {
         "id": 2,
-        "titulo": "Produto 2",
-        "descricao": "Este é um excelente produto 2.",
+        "titulo": "Closet",
+        "descricao": "Closet 3 Gavetas 7 Nichos 2 Cabideiros 1 Maleiro 236x159x54cm Legno Crema Prime Luciane",
         "valor": 150.00,
-        "disponivel": false,
+        "disponivel": true,
+        "quantidade": 2,
         "dataEntrega": "05/01/2025",
         "estado": "Usado",
         "categoria": "Móveis",
-        "imagens": ["poltrona-1.jpg", "poltrona-1.jpg", "poltrona-1.jpg", "poltrona-1.jpg", "poltrona-1.jpg"],
-        "whatsapp": "https://wa.me/5511999999998"
+        "imagens": ["01-closet-1.jpg", "01-closet-2.jpg"]
     },
-    {
-        "id": 3,
-        "titulo": "Produto 2",
-        "descricao": "Este é um excelente produto 2.",
-        "valor": 150.00,
-        "disponivel": false,
-        "dataEntrega": "05/01/2025",
-        "estado": "Usado",
-        "categoria": "Eletrônicos",
-        "imagens": ["switch-1.jpg", "switch-1.jpg", "switch-1.jpg", "switch-1.jpg", "switch-1.jpg"],
-        "whatsapp": "https://wa.me/5511999999998"
-    },
-    {
-        "id": 4,
-        "titulo": "Produto 2",
-        "descricao": "Este é um excelente produto 2.",
-        "valor": 150.00,
-        "disponivel": false,
-        "dataEntrega": "05/01/2025",
-        "estado": "Usado",
-        "categoria": "Eletrônicos",
-        "imagens": ["volante-g23-1.jpg", "volante-g23-1.jpg", "volante-g23-1.jpg", "volante-g23-1.jpg", "volante-g23-1.jpg"],
-        "whatsapp": "https://wa.me/5511999999998"
-    },
-    {
-        "id": 5,
-        "titulo": "Produto 2",
-        "descricao": "Este é um excelente produto 2.",
-        "valor": 150.00,
-        "disponivel": false,
-        "dataEntrega": "05/01/2025",
-        "estado": "Usado",
-        "categoria": "Eletrônicos",
-        "imagens": ["ipad-1.jpg", "ipad-2.jpg", "ipad-3.jpg", "ipad-4.jpg", "ipad-4.jpg"],
-        "whatsapp": "https://wa.me/5511999999998"
-    },
-    {
-        "id": 6,
-        "titulo": "Produto 2",
-        "descricao": "Este é um excelente produto 2.",
-        "valor": 150.00,
-        "disponivel": false,
-        "dataEntrega": "05/01/2025",
-        "estado": "Usado",
-        "categoria": "Eletrônicos",
-        "imagens": ["ipad-1.jpg", "ipad-2.jpg", "ipad-3.jpg", "ipad-4.jpg", "ipad-4.jpg"],
-        "whatsapp": "https://wa.me/5511999999998"
-    }
 ];
